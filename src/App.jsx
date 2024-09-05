c95c52823f2d3d62bd3e8b85372971cfe8594945
huyTrinhVAn <trinhvanhuy11a9@gmail.com>
2024-09-05 22:19:03 +1000

    Update App and HeroSection, add FeatureSection and WorkFlow components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
+import WorkFlow from './components/WorkFlow'
 const App = () => {
   return (
     <div>
       <Navbar />
       <div className='max-w-7xl mx-auto pt-20 px-6'>
         <HeroSection />
+        <FeatureSection />
+        <WorkFlow />
       </div>
 
     </div >
